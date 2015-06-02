ab9fb8da74eb28a52b73b0d5edd181404b541b04
BiohzrdMX <biohazardmx@gmail.com>
2015-06-02 11:14:34 -0500

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,178 @@
-# jQuery.Tourette
+## Tourette
+
 Meet Tourette. An advanced tour engine for web apps.
+
+### Why Tourette?
+
+From Wikipedia:
+
+_"Tourette syndrome [...] is an inherited neuropsychiatric disorder with onset in childhood, characterized by multiple physical (motor) tics and at least one vocal (phonic) tic. These tics characteristically wax and wane, can be suppressed temporarily, and are preceded by a premonitory urge. Tourette's is defined as part of a spectrum of tic disorders, which includes provisional, transient and persistent (chronic) tics."_
+
+And this plugin provides a solid platform for page **tours**...
+
+...that show you some elements and can trigger **automated events** (open menus, tick checkboxes, fill text areas, etc), effectively **taking control** of the browser...
+
+...so Tourette is darn good name!
+
+### How to use
+
+How do you create your own tours?
+
+It's pretty simple. First make sure to include jQuery (1.8+ would do) and both Tourette's JS and CSS files
+
+**CSS**
+
+`<link rel="stylesheet" href="css/tourette.css">`
+
+**JS**
+
+`<script type="text/javascript" src="js/jquery.min.js"></script>`
+
+`<script type="text/javascript" src="js/tourette.js"></script>`
+
+Then build your great web app or web site, you just need some markup to play with.
+
+And here's the interesting part: _create your tour definition file_
+
+#### The tour definition file
+
+This is the 'map' to your landmarks: each landmark represents an stop in the tour and can be related to a page element.
+
+The basic structure of a tour is a very simple JSON:
+
+	{
+		"name":"Sample tour",
+		"author":"Your name, email, etc",
+		"landmarks":[]
+	}
+
+As you may have guessed, `landmarks` will hold each of our tour stops, based on the following structure:
+
+	{
+		"name":"landmark-name",
+		"title":"Landmark Title",
+		"contents":"The text to be shown on the prompt, it may include HTML tags, just try to keep it simple and concise.",
+		"element":".element-selector",
+		"actions":[]
+	}
+
+Landmarks may have a related page element or not: you can put `null` on `element` or a jQuery selector to match the desired element.
+
+And then come the actions. Each prompt can hace one or more buttons to navigate the tour or to trigger custom events/interactions.
+
+Actions are defined by the following structure:
+
+	{
+		"text":"Button text",
+		"cssClass":".button-class",
+		"action":"Button.action"
+	}
+
+Tourette comes with some predefined `actions`:
+
+*   `Tourette.next` Jump to the next landmark
+*   `Tourette.prev` Jump to the previous landmark
+*   `Tourette.show` Show the first landmark
+*   `Tourette.hide` Quit the tour
+
+If you want to do other things, you may put a custom action name and use it through the `onButton` callback. More on this below.
+
+If you want to use custom classes on each button, fill the _optional_ `cssClass` item.
+
+#### Loading and running the tour
+
+Once you have your tour definition ready you will need to initialize Tourette:
+
+	var tourette = $.Tourette({ /* options */ }); // Construct a new Tourette object
+
+Then just load your tour file and start the tour. Is that easy!
+
+	$.ajax({
+		url: 'tour.json',
+		dataType: 'json',
+		success: function(response) {
+			tourette.load(response);
+			tourette.start();
+		}
+	});
+
+#### Constructor options
+
+You may customize the markup of Tourette elements as well as specify callbacks to know when something happens:
+
+*   `overlayMarkup` The overlay markup, defaults to `<div class="tourette-overlay"></div>`
+*   `promptMarkup` The prompt markup, defaults to `<div class="tourette-prompt"></div>`
+*   `buttonMarkup` The button markup, defaults to `<a href="#" class="tourette-button"></a>`
+*   `onShow` onShow callback (see the next section)
+*   `onHide` onHide callback (see the next section)
+*   `onStart` onStart callback (see the next section)
+*   `onExit` onExit callback (see the next section)
+*   `onButton` onButton callback (see the next section)
+
+For example, to use Bootstrap buttons you may do:
+
+	var tourette = $.Tourette({
+		buttonMarkup: '<a href="#" class="tourette-button btn btn-primary"></a>'
+	});
+
+
+#### Callbacks
+
+Tourette has an useful set of callbacks that will help you to create awesome page tours easily:
+
+`onShow(landmark)`
+
+Called when the prompt is shown
+ Receives the current `landmark` object
+
+`onHide(landmark)`
+
+Called when the prompt is hidden
+ Receives the current `landmark` object
+
+`onStart(landmark)`
+
+Called when the tour is started
+ Receives the current `landmark` object
+
+`onExit(landmark)`
+
+Called when the tour has ended
+ Receives the current `landmark` object
+
+`onButton(landmark, action, button)`
+
+Called when a button with a custom action is clicked
+ Receives the current `landmark`, `action` and `button` objects
+
+To receive callbacks, just pass an options object to the Tourette constructor:
+
+	var tourette = $.Tourette({
+		onExit: function(landmark) {
+			alert('You just ended the tour');
+		}
+	});
+
+### Troubleshooting
+
+Tourette _should work_ on major, modern browsers and has some mobile support. I've tested it on Firefox and Chrome/Opera. Internet Explorer should do fine, but it may have weird bugs.
+
+TL;DR version, use a modern browser and you should be fine.
+
+However, if you've found a bug, please open an issue. Or better yet, fix it and send me a pull request :)
+
+### Credits
+
+**Lead coder:** biohzrdmx [<github.com/biohzrdmx>](http://github.com/biohzrdmx)
+
+### License
+
+The MIT License (MIT)
+
+Copyright (c) 2015 biohzrdmx
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ js/tourette.js
@@ -0,0 +1,289 @@
+(function($) {
+	// What does the tourette plugin do?
+	$.Tourette = function(options) {
+		var tourette = {
+			tour: null,
+			position: null,
+			current: null,
+			elements: null,
+			templates: null,
+			prevState: null,
+			defaults: {
+				overlayMarkup: '<div class="tourette-overlay"></div>',
+				promptMarkup: '<div class="tourette-prompt"></div>',
+				buttonMarkup: '<a href="#" class="tourette-button"></a>',
+				onShow: function(landmark) {
+					/* Placeholder */
+				},
+				onHide: function(landmark) {
+					/* Placeholder */
+				},
+				onStart: function(landmark) {
+					/* Placeholder */
+				},
+				onExit: function(landmark) {
+					/* Placeholder */
+				},
+				onButton: function(landmark, action, button) {
+					/* Placeholder */
+				}
+			},
+			callbacks: null,
+			init: function(options) {
+				var obj = this,
+					opts = $.extend(true, {}, obj.defaults, options);
+				// Save element templates
+				obj.templates = {
+					overlay: opts.overlayMarkup,
+					prompt: opts.promptMarkup,
+					button: opts.buttonMarkup
+				};
+				obj.callbacks = {
+					onShow: opts.onShow,
+					onHide: opts.onHide,
+					onStart: opts.onStart,
+					onExit: opts.onExit,
+					onButton: opts.onButton
+				};
+				// Generate DOM elements
+				obj.elements = {
+					overlay: $(obj.templates.overlay)
+				};
+				// And add it to the DOM
+				obj.elements.overlay.hide();
+				$('body').append(obj.elements.overlay);
+				//
+				return obj;
+			},
+			load: function(tour) {
+				var obj = this;
+				// Initialize stuff
+				obj.tour = tour || [];
+				obj.position = 0;
+				obj.current = null;
+			},
+			start: function() {
+				var obj = this;
+				// Check for a valid landmark
+				if ( typeof obj.tour.landmarks[obj.position] !== 'undefined' ) {
+					// Get the first landmark
+					obj.current = obj.tour.landmarks[obj.position];
+					obj.callbacks.onStart(obj.current);
+					// And start the show
+					obj.show();
+				} else {
+					// No landmarks
+				}
+			},
+			next: function() {
+				var obj = this;
+				// Move to the next landmark
+				obj.position++;
+				// Clip the position pointer
+				obj.position = obj.position >= obj.tour.landmarks.length ? obj.tour.landmarks.length - 1 : obj.position;
+				// Check for a valid landmark
+				if ( typeof obj.tour.landmarks[obj.position] !== 'undefined' ) {
+					// Get the landmark
+					obj.callbacks.onHide(obj.current);
+					obj.current = obj.tour.landmarks[obj.position];
+					obj.callbacks.onShow(obj.current);
+					// Profit!
+					obj.show();
+				} else {
+					// No landmarks
+				}
+			},
+			prev: function() {
+				var obj = this;
+				// Move to the previous landmark
+				obj.position--;
+				// Clip the position pointer
+				obj.position = obj.position < 0 ? 0 : obj.position;
+				// Check for a valid landmark
+				if ( typeof obj.tour.landmarks[obj.position] !== 'undefined' ) {
+					// Get the landmark
+					obj.callbacks.onHide(obj.current);
+					obj.current = obj.tour.landmarks[obj.position];
+					obj.callbacks.onShow(obj.current);
+					// Yay!
+					obj.show();
+				} else {
+					// No landmarks
+				}
+			},
+			show: function() {
+				var obj = this;
+				if ( typeof obj.current !== 'undefined' ) {
+					// Show the overlay if it isn't visible yet
+					if (! obj.elements.overlay.is(':visible') ) {
+						obj.elements.overlay.fadeIn();
+					}
+					// Define our prompt factory function
+					var createPrompt = function() {
+						var prompt = $(obj.templates.prompt);
+						// Create a base prompt
+						obj.elements.prompt = prompt;
+						obj.elements.prompt.append('<h1 class="tourette-title">'+ obj.current.title +'</h1>');
+						obj.elements.prompt.append('<div class="tourette-contents">'+obj.current.contents+'</div>');
+						// Now create the buttons container
+						var actions = $('<div class="tourette-actions"></div>');
+						for (var i = 0; i < obj.current.actions.length; i++) {
+							(function(action) {
+								var button = $(obj.templates.button);
+								// Format the button
+								button.text(action.text);
+								if (action.cssClass) {
+									button.attr('class', action.cssClass);
+								}
+								// And listen to its click event
+								button.on('click', function(e) {
+									e.preventDefault();
+									// Choose from the preset actions
+									switch (action.action) {
+										case 'Tourette.next':
+											obj.next();
+											break;
+										case 'Tourette.prev':
+											obj.prev();
+											break;
+										case 'Tourette.show':
+											obj.show();
+											break;
+										case 'Tourette.hide':
+											obj.hide();
+											break;
+										default:
+											obj.callbacks.onButton(obj.current, action, button);
+									}
+								});
+								// Add the button to the container
+								actions.append(button);
+							})(obj.current.actions[i]);
+						};
+						// And add the buttons container
+						obj.elements.prompt.append(actions);
+						// Hide the prompt and add it to the DOM
+						obj.elements.prompt.hide();
+						$('body').append(obj.elements.prompt);
+						// Now get the actual element and calculate the prompt dimensions
+						var element = $(obj.current.element),
+							promptWidth = prompt.outerWidth(),
+							promptHeight = prompt.outerHeight();
+						// Check if the element has a valid object
+						if ( obj.current.element && element.length ) {
+							obj.moveToElement(element);
+						} else {
+							// No element, we must center the prompt
+							prompt.css({ top: 50, left: '50%', marginLeft: -(promptWidth / 2) + 'px' });
+							// Scroll to the top
+							$('html, body').animate({
+								scrollTop: 0
+							}, 400, function() {
+								// And show the prompt
+								obj.elements.prompt.fadeIn();
+							});
+						}
+					}
+					// Now to the actual processing
+					if ( obj.elements.prompt ) {
+						// Previous prompt, hide it, kill it (with fire) and create a new one
+						obj.elements.prompt.fadeOut(function() {
+							$(this).remove();
+							// But do it after a brief pause
+							setTimeout(createPrompt, 400);
+						});
+					} else {
+						// No other prompts in sight, create it directly
+						createPrompt();
+					}
+				} else {
+					// No current landmark
+				}
+			},
+			hide: function() {
+				var obj = this;
+				// Hide callback
+				obj.callbacks.onHide(obj.current);
+				// Hiding the prompt
+				obj.elements.prompt.fadeOut(function() {
+					// Kill it
+					$(this).remove();
+				});
+				// Restore stuff
+				if (obj.prevState) {
+					// Reset previous element properties
+					obj.prevState.element.css({ position: obj.prevState.styles.position, zIndex: obj.prevState.styles.zIndex });
+					obj.prevState = null;
+				}
+				// Hide the overlay
+				obj.elements.overlay.fadeOut();
+				// And go to the top
+				$('html, body').animate({
+					scrollTop: 0
+				}, 400);
+				// Exit callback
+				obj.callbacks.onExit(obj.current);
+			},
+			moveToElement: function(element) {
+				// Get the dimensions of the viewport and the element
+				var obj = this,
+					elementPos = element.offset(),
+					elementWidth = element.outerWidth(),
+					elementHeight = element.outerHeight(),
+					viewWidth = $(window).width(),
+					viewHeight = $(window).height(),
+					prompt = obj.elements.prompt,
+					promptWidth = prompt.outerWidth(),
+					promptHeight = prompt.outerHeight(),
+					scrollPos = 0,
+					promptPos = {
+						left: 0,
+						top: 0
+					};
+				// Check whether we have a highlighted element to reset or not
+				if (obj.prevState) {
+					console.log(obj.prevState);
+					// Reset previous element properties
+					obj.prevState.element.css({ position: obj.prevState.styles.position, zIndex: obj.prevState.styles.zIndex });
+					obj.prevState = null;
+				}
+				// Save current element state
+				obj.prevState = {
+					element: element,
+					styles: {
+						position: element.css('position'),
+						zIndex: element.css('zIndex')
+					}
+				}
+				// Change position to relative, if required
+				element.css({ position: obj.prevState.styles.position == 'absolute' ? obj.prevState.styles.position : 'relative', zIndex: 2100 });
+				// Try to fit the prompt vertically
+				if (elementPos.top > promptHeight + 15 && !obj.current.forceBelow) {
+					// Prompt fits above the element
+					prompt.addClass('prompt-above');
+					promptPos.top = elementPos.top - (promptHeight + 15);
+					scrollPos = promptPos.top - 15;
+				} else {
+					// Prompt fits below the element
+					prompt.addClass('prompt-below');
+					promptPos.top = elementPos.top + elementHeight + 15;
+					scrollPos = elementPos.top - 15;
+				}
+				// Try to fit the prompt horizontally
+				promptPos.left = elementPos.left + ((elementWidth / 2) - (promptWidth / 2));
+				promptPos.left = promptPos.left < 0 ? 0 : (promptPos.left + promptWidth > viewWidth ? viewWidth - promptWidth : promptPos.left);
+				// Position the prompt
+				prompt.css(promptPos);
+				// Scroll to the adequate position
+				$('html, body').animate({
+					scrollTop: scrollPos
+				}, 400, function() {
+					// And show the prompt
+					obj.elements.prompt.fadeIn();
+				});
+			}
+		};
+		// Call constructor and return instance
+		return tourette.init(options);
+	};
+})(jQuery);
